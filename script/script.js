1923239b26308483b0b96b73e152b8e85cf995c6
Denis Sidrikov <Denis_06.95@mail.ru>
2020-04-06 02:53:19 +0500

    первый комит

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let money = 100;
+let income = 'freelance';
+let addExpenses ='Internet, Taxi, Food';
+let deposit = true;
+let mission = 100000;
+const period = 12;
+let budgetDay = 30;
+console.log(typeof money,income,deposit);
+console.log(addExpenses.length);
+console.log("Период равен"+ " "+ period +" " +"месяцев"+"   "+" Цель заработать"+" " + mission+ " " + "рублей");
+console.log(addExpenses.toUpperCase());
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log("доход за месяц / "+ budgetDay);
+
