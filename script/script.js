88326e2fd5fc25de55779ba32afb68ef7cf61fd4
Denis Sidrikov <Denis_06.95@mail.ru>
2020-04-06 17:36:44 +0500

    Исправил, свою невнимательность

--- script/script.js
+++ script/script.js
@@ -4,12 +4,13 @@ let addExpenses ='Internet, Taxi, Food';
 let deposit = true;
 let mission = 100000;
 const period = 12;
-let budgetDay = 30;
-console.log(typeof money,income,deposit);
+let budgetDay = money/30;
+console.log(typeof money,typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log("Период равен"+ " "+ period +" " +"месяцев"+"   "+" Цель заработать"+" " + mission+ " " + "рублей");
 console.log(addExpenses.toUpperCase());
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
-console.log("доход за месяц / "+ budgetDay);
+console.log( budgetDay);
+console.log("dsdsdf");
 
